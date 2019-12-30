4c7a8421ba2fde15652fc131a0b3aeb68c7febf9
Chris <340019109@qq.com>
2019-12-30 08:56:07 +0800

    【WebSocket Test】调试更新

--- TestServer/WSClient/wsclient.js
+++ TestServer/WSClient/wsclient.js
@@ -2,7 +2,7 @@
 
 var WebSocket = require('ws')
 // var ws = new WebSocket("wss://echo.websocket.org");
-var ws = new WebSocket("ws://127.0.0.1:8887");
+var ws = new WebSocket("ws://172.20.10.6:60009");
 
 ws.onopen = function(evt) {
     console.log("Connection open ...");
--- TestServer/WSServer/wsserver.js
+++ TestServer/WSServer/wsserver.js
@@ -1,7 +1,12 @@
 var WebSocketServer = require("ws").Server;
-var wss = new WebSocketServer({ port: 8887 });
+var wss = new WebSocketServer({ port: 60009 });
 
 wss.on("connection", function(ws) {
-	ws.send("Welcome to cyber chat");
 
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        ws.send("收到");
+    });
+
+	ws.send("Welcome to cyber chat");
 });
